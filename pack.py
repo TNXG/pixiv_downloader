fe42d837a4092c02b2570fb9949c5ef42f6f7196
天翔TNXG <69001561+TNXG@users.noreply.github.com>
2022-07-19 12:12:52 +0000

    修改一键脚本

--- pack.py
+++ pack.py
@@ -4,7 +4,9 @@ import os
 now = datetime.datetime.now()
 nowtime = now.strftime("%Y-%m-%d")
 url = 'Pixiv日榜/' + nowtime
-os.system('cd '+ url)
-os.system('zip -q -r ../' + nowtime + '.zip /* && cd ../')
-os.system('rm -r '+ nowtime)
-os.system('cd ../ && python upload_ali.py')
+os.system('echo \'cd '+url+' && zip -r ../' +nowtime+'.zip * && cd ../\' > other.sh')
+os.system('echo \'rm -r ' + nowtime+'\' >> other.sh')
+os.system('echo \'cd ../ && python upload_ali.py\' >> other.sh')
+os.system('rm -r Pixiv日榜')
+os.system('sh other.sh')
+os.system('rm -r other.sh')
